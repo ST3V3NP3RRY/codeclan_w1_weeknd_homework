60c36da517e9b1515c1f3220ea5a9a10b80b3838
Steven perry <Sperry3006@gmail.com>
2022-09-16 22:04:31 +0100

    second commit completed get_customer_cash test

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -49,3 +49,13 @@ def remove_pet_by_name(pet_shop, name):
 
 def add_pet_to_stock(pet_shop, new_pet):
     pet_shop["pets"].append(new_pet)
+
+
+def get_customer_cash(customers):
+    customer_cash = customers["cash"]
+    return customer_cash
+
+
+# Heres where I am at
+def remove_customer_cash():
+    pass
