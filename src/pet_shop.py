fcddc66e205df8f765dbdccb52ac6366467de314
Steven perry <Sperry3006@gmail.com>
2022-09-19 14:13:44 +0100

    refactored last function still one test left to pass

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -72,30 +72,17 @@ def customer_can_afford_pet(customer, new_pet):
 
 
 def sell_pet_to_customer(pet_shop, pet, customer):
-    # # Is the pet name in the pet_shop?
-    pet_names = []
 
-    for pets in pet_shop["pets"]:
-        pet_names.append(pets["name"])
-    # print(pet_names)
+    for animals in pet_shop["pets"]:
+        if pet["name"] == animals["name"] and customer["cash"] >= pet["price"]:
 
-    if pets["name"] in pet_names and customer["cash"] >= pet["price"]:
+            customer["pets"].append(pet["name"])
 
-        customer["pets"].append(pet)
+            pet_shop["admin"]["pets_sold"] += len(customer["pets"])
 
-        pet_shop["admin"]["pets_sold"] += 1
+            customer["cash"] -= pet["price"]
 
-        customer["cash"] -= pet["price"]
+            pet_shop["admin"]["total_cash"] += pet["price"]
 
-        pet_shop["admin"]["total_cash"] += pet["price"]
-    # for pets in pet_shop["pets"]:
-    #     if pet == pets["name"] and customer["cash"] >= pet["price"]:
-    #         customer["pets"].append(pet)
-
-    print(customer["pets"])
-    print("----------")
-    print(pet)
-    print("----------")
-    print(pets["name"])
-    print("----------")
-    print(customer["cash"])
+        if pet["name"] != animals["name"]:
+            return None
